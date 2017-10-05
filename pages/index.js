422879e839aed2f174593070b74e113e8394323e
Rafael Fragoso <rafaelfragosom@gmail.com>
2017-10-05 19:51:08 -0300

    Adding a reusable Header

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-import stylesheet from '../styles/index.scss';
-import Head from 'next/head';
+import Header from '../components/header';
 
 export default () => 
 	<div>
-		<Head>
-			<style dangerouslySetInnerHTML={{ __html: stylesheet }} />
-		</Head>
-		<div>Bem-vindos!</div>
+		<Header 
+			title="Open Source - Globo.com"
+			description="A globo.com ama a comunida open source! Conheça e contribua para os nossos projetos." />
+		<div className="columns">
+			<div className="column">Ola</div>
+			<div className="column">Ola 2</div>
+		</div>
 	</div>
