bb2e71f1ad13d0670efe5f688aa4c75e72c5e45f
Artur Sousa <artur.sousa@corp.globo.com>
2017-10-10 19:09:53 -0300

    Add an .editorconfig file

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Header from '../components/header';
 
-export default () => 
+export default () =>
 	<div>
-		<Header 
+		<Header
 			title="Open Source - Globo.com"
 			description="A globo.com ama a comunida open source! Conheça e contribua para os nossos projetos." />
 		<div className="columns">
