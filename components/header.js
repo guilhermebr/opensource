632ebb398a3ff18ea826db805188486f0cadc653
Artur Sousa <artur.sousa@corp.globo.com>
2017-10-11 03:10:30 -0300

    Add a bulma navbar style

--- components/header.js
+++ components/header.js
@@ -2,22 +2,35 @@ import Link from 'next/link';
 
 export default () =>
   <header>
-    <Link href="/index">
-      <a>globo.com Open source</a>
-    </Link>
-    <Link href="/projects">
-      <a>Projetos</a>
-    </Link>
-    <Link href="/team">
-      <a>Time</a>
-    </Link>
-    <Link href="/opportunities">
-      <a>Oportunidades</a>
-    </Link>
-    <Link href="/events">
-      <a>Eventos</a>
-    </Link>
-    <Link href="/hacktober">
-      <a>Hacktober</a>
-    </Link>
+    <nav className='navbar'>
+      <div className='navbar-brand'>
+        <Link href='/index'>
+          <a className='navbar-item'>globo.com Open source</a>
+        </Link>
+        <div className='navbar-burger burger'>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <div className='navbar-menu'>
+        <div className='navbar-end'>
+          <Link href='/projects'>
+            <a className='navbar-item'>Projetos</a>
+          </Link>
+          <Link href='/team'>
+            <a className='navbar-item'>Time</a>
+          </Link>
+          <Link href='/opportunities'>
+            <a className='navbar-item'>Oportunidades</a>
+          </Link>
+          <Link href='/events'>
+            <a className='navbar-item'>Eventos</a>
+          </Link>
+          <Link href='/hacktober'>
+            <a className='navbar-item'>Hacktober</a>
+          </Link>
+        </div>
+      </div>
+    </nav>
   </header>
