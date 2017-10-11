78b4b3c2e21307d1fb1e406bd640fed6a7e4f5d5
Artur Sousa <artur.sousa@corp.globo.com>
2017-10-11 02:44:32 -0300

    Add some links for testing routing

--- components/header.js
+++ components/header.js
@@ -1,5 +1,23 @@
+import Link from 'next/link';
 
 export default () =>
   <header>
-    THIS IS THE HEADER
+    <Link href="/index">
+      <a>globo.com Open source</a>
+    </Link>
+    <Link href="/projects">
+      <a>Projetos</a>
+    </Link>
+    <Link href="/team">
+      <a>Time</a>
+    </Link>
+    <Link href="/opportunities">
+      <a>Oportunidades</a>
+    </Link>
+    <Link href="/events">
+      <a>Eventos</a>
+    </Link>
+    <Link href="/hacktober">
+      <a>Hacktober</a>
+    </Link>
   </header>
